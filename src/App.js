a3ad745dd1b4e9c71690b1f132afc4058dd472c9
nguyentrong1121 <nguyentrong1121@gmail.com>
2022-03-15 13:13:19 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import { BrowserRouter as Router, Route, Link, NavLink, BrowserRouter, Switch } from "react-router-dom";
+import {BrowserRouter as Router, Route, Link, NavLink, BrowserRouter, Switch} from "react-router-dom";
 import Home from "./component/views/home/Home"
 import Shop from "./component/views/shop/Shop"
 import SideBar from "./component/layout/SideBar"
 import Products from "./component/products/Products"
 import Categories from "./component/categories/Categories";
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
 import Header from "./component/layout/Header";
 import Footer from "./component/layout/Footer";
 import Detail from "./component/views/detail/Detail";
@@ -13,78 +13,92 @@ import Checkout from "./component/views/checkout/Checkout";
 import callApi from "./component/callAPI/apiCaller";
 import Users from "./component/users/Users";
 import Login from "./component/views/login/Login";
-import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
+import {QueryClient, QueryClientProvider, useQuery} from 'react-query'
+import {useAuth} from "./context";
+import Storage from "./utils/Storage";
+
 const queryClient = new QueryClient()
+
 function App() {
-  const [product, setProduct] = useState([]);
-  const [category, setCategory] = useState([]);
-  const [user, setUser] = useState([]);
-  const [role, setRole] = useState([]);
-  const [loading, setLoading] = useState(false);
-  return (
-      <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
-      <Switch>
-        <Route path="/admin">
-          <div className="App d-flex h-100">
-            <SideBar
-              category={category}
-            />
-            <Route path="/admin/categories">
-              <Categories
-                category={category}
-                setCategory={setCategory}
-                loading={loading}
-                setLoading={setLoading}
-              />
-            </Route>
-            <Route path="/admin/products">
-              <Products
-                product={product}
-                setProduct={setProduct}
-                loading={loading}
-                setLoading={setLoading}
-                category={category}
-              />
-            </Route>
-            <Route path="/admin/users">
-              <Users
-                user={user}
-                setUser={setUser}
-                loading={loading}
-                setLoading={setLoading}
-                role={role}
-              />
-            </Route>
-          </div>
-        </Route>
-        <Route>
-          <Header />
-          <Switch>
-            <Route path="/" exact>
-              <Home />
-            </Route>
-            <Route path="/shop" exact>
-              <Shop/>
-            </Route>
-            <Route path="/login" exact>
-              <Login/>
-            </Route>
-            <Route path="/detail/:id" exact>
-              <Detail />
-            </Route>
-            <Route path="/cart" exact>
-              <Cart />
-            </Route>
-            <Route path="/checkout" exact>
-              <Checkout />
-            </Route>
-          </Switch>
-          <Footer/>
-        </Route>
-      </Switch>
-    </BrowserRouter>
-      </QueryClientProvider>
-  )
+    const {setUserInfo} = useAuth()
+    const [product, setProduct] = useState([]);
+    const [category, setCategory] = useState([]);
+    const [user, setUser] = useState([]);
+    const [role, setRole] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(()=>{
+        let userData = Storage.get('userData')
+        if(userData){
+            setUserInfo(userData)
+        }
+    },[])
+
+    return (
+        <QueryClientProvider client={queryClient}>
+                <BrowserRouter>
+                    <Switch>
+                        <Route path="/admin">
+                            <div className="App d-flex h-100">
+                                <SideBar
+                                    category={category}
+                                />
+                                <Route path="/admin/categories">
+                                    <Categories
+                                        category={category}
+                                        setCategory={setCategory}
+                                        loading={loading}
+                                        setLoading={setLoading}
+                                    />
+                                </Route>
+                                <Route path="/admin/products">
+                                    <Products
+                                        product={product}
+                                        setProduct={setProduct}
+                                        loading={loading}
+                                        setLoading={setLoading}
+                                        category={category}
+                                    />
+                                </Route>
+                                <Route path="/admin/users">
+                                    <Users
+                                        user={user}
+                                        setUser={setUser}
+                                        loading={loading}
+                                        setLoading={setLoading}
+                                        role={role}
+                                    />
+                                </Route>
+                            </div>
+                        </Route>
+                        <Route>
+                            <Header/>
+                            <Switch>
+                                <Route path="/" exact>
+                                    <Home/>
+                                </Route>
+                                <Route path="/shop" exact>
+                                    <Shop/>
+                                </Route>
+                                <Route path="/login" exact>
+                                    <Login/>
+                                </Route>
+                                <Route path="/detail/:id" exact>
+                                    <Detail/>
+                                </Route>
+                                <Route path="/cart" exact>
+                                    <Cart/>
+                                </Route>
+                                <Route path="/checkout" exact>
+                                    <Checkout/>
+                                </Route>
+                            </Switch>
+                            <Footer/>
+                        </Route>
+                    </Switch>
+                </BrowserRouter>
+        </QueryClientProvider>
+    )
 }
+
 export default App;
--- src/component/views/login/Login.js
+++ src/component/views/login/Login.js
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from "react";
 import callApi from "../../callAPI/apiCaller";
+import Storage from '../../../utils/Storage'
+import {useAuth} from "../../../context";
 
 
 function Login() {
+
+    const {setUserInfo} = useAuth()
     
     const [formData, setFormData] = useState({
         username: null,
@@ -11,6 +15,7 @@ function Login() {
 
     const onSubmitHandler = (event) => {
         event.preventDefault();
+        console.log(JSON.parse(localStorage.getItem('userData')))
         login()
     };
     
@@ -25,18 +30,21 @@ function Login() {
     const login = () =>{
         callApi('authenticate','POST',formData)
         .then(res => {
-            console.log(res);
+            if(res?.data){
+                Storage.save('userData', res?.data)
+                setUserInfo(res.data)
+            }
         })
     }
 
     return (
         <div>
-            <div class="slider-area ">
-                <div class="single-slider slider-height2 d-flex align-items-center">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-xl-12">
-                                <div class="hero-cap text-center">
+            <div className="slider-area ">
+                <div className="single-slider slider-height2 d-flex align-items-center">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-xl-12">
+                                <div className="hero-cap text-center">
                                     <h2>Login</h2>
                                 </div>
                             </div>
@@ -44,40 +52,40 @@ function Login() {
                     </div>
                 </div>
             </div>
-            <section class="login_part section_padding ">
-                <div class="container">
-                    <div class="row align-items-center">
-                        <div class="col-lg-6 col-md-6">
-                            <div class="login_part_text text-center">
-                                <div class="login_part_text_iner">
+            <section className="login_part section_padding ">
+                <div className="container">
+                    <div className="row align-items-center">
+                        <div className="col-lg-6 col-md-6">
+                            <div className="login_part_text text-center">
+                                <div className="login_part_text_iner">
                                     <h2>New to our Shop?</h2>
                                     <p>There are advances being made in science and technology
                                         everyday, and a good example of this is the</p>
-                                    <a href="#" class="btn_3">Create an Account</a>
+                                    <a href="#" className="btn_3">Create an Account</a>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-lg-6 col-md-6">
-                            <div class="login_part_form">
-                                <div class="login_part_form_iner">
+                        <div className="col-lg-6 col-md-6">
+                            <div className="login_part_form">
+                                <div className="login_part_form_iner">
                                     <h3>Welcome Back ! <br />
                                         Please Sign in now</h3>
-                                    <form class="row contact_form" action="#" method="post" novalidate="novalidate" onSubmit={onSubmitHandler} >
-                                        <div class="col-md-12 form-group p_star">
-                                            <input onChange={onChangeHandler} value={formData.username} type="text" class="form-control" id="name" name="username" placeholder="Username" />
+                                    <form className="row contact_form" action="#" method="post" novalidate="novalidate" onSubmit={onSubmitHandler} >
+                                        <div className="col-md-12 form-group p_star">
+                                            <input onChange={onChangeHandler} value={formData.username} type="text" className="form-control" id="name" name="username" placeholder="Username" />
                                         </div>
-                                        <div class="col-md-12 form-group p_star">
-                                            <input onChange={onChangeHandler} value={formData.password} type="password" class="form-control" id="password" name="password" placeholder="Password" />
+                                        <div className="col-md-12 form-group p_star">
+                                            <input onChange={onChangeHandler} value={formData.password} type="password" className="form-control" id="password" name="password" placeholder="Password" />
                                         </div>
-                                        <div class="col-md-12 form-group">
-                                            <div class="creat_account d-flex align-items-center">
+                                        <div className="col-md-12 form-group">
+                                            <div className="creat_account d-flex align-items-center">
                                                 <input type="checkbox" id="f-option" name="selector" />
                                                 <label for="f-option">Remember me</label>
                                             </div>
-                                            <button type="submit" value="submit" class="btn_3">
+                                            <button type="submit" value="submit" className="btn_3">
                                                 log in
                                             </button>
-                                            <a class="lost_pass" href="#">forget password?</a>
+                                            <a className="lost_pass" href="#">forget password?</a>
                                         </div>
                                     </form>
                                 </div>
